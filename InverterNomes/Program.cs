c35ccae5aea129d4d7dada8a7ed41faced334a0f
Renor Oliveira Guimarães <renornew@gmail.com>
2022-11-15 22:11:55 -0300

    Refatoração de código usando lista e metodo

--- InverterNomes/Program.cs
+++ InverterNomes/Program.cs
@@ -1,31 +1,49 @@
-﻿string nome1, nome2, nome3, nome4, auxiliar;
+﻿using System.ComponentModel;
+// Declaração da lista
+List<string> nomePessoas = new List<string>();
 
-Console.WriteLine("Digite o nome #1: ");
-nome1 = Console.ReadLine();
+for (int i = 1; i <= 4; i++)
+{
+    Console.WriteLine($"Digite o nome #{i}: ");
 
-Console.WriteLine("Digite o nome #2: ");
-nome2 = Console.ReadLine();
-
-Console.WriteLine("Digite o nome #3: ");
-nome3 = Console.ReadLine();
-
-Console.WriteLine("Digite o nome #4: ");
-nome4 = Console.ReadLine();
-
-// Mecanismo para inverter os nomes
-auxiliar = nome1;
-nome1 = nome4;
-nome4 = auxiliar;
-auxiliar = nome2;
-nome2 = nome3;
-nome3 = auxiliar;
+    string nome = Console.ReadLine();
+    // Adiciona o item à lista
+    nomePessoas.Add(nome);
+    
+}
 
 Console.WriteLine();
 
-Console.WriteLine("Nomes inseridos na sequência invertida.");
-Console.WriteLine(nome1);
-Console.WriteLine(nome2);
-Console.WriteLine(nome3);
-Console.WriteLine(nome4);
+nomePessoas.Reverse();
 
-Console.ReadKey();
+nomePessoas.ForEach(i => Console.WriteLine(i));
+
+    //Console.WriteLine("Digite o nome #1: ");
+    //nome1 = Console.ReadLine();
+
+    //Console.WriteLine("Digite o nome #2: ");
+    //nome2 = Console.ReadLine();
+
+    //Console.WriteLine("Digite o nome #3: ");
+    //nome3 = Console.ReadLine();
+
+    //Console.WriteLine("Digite o nome #4: ");
+    //nome4 = Console.ReadLine();
+
+    // Mecanismo para inverter os nomes
+    //auxiliar = nome1;
+    //nome1 = nome4;
+    //nome4 = auxiliar;
+    //auxiliar = nome2;
+    //nome2 = nome3;
+    //nome3 = auxiliar;
+
+    //Console.WriteLine();
+
+    //Console.WriteLine("Nomes inseridos na sequência invertida.");
+    //Console.WriteLine(nome1);
+    //Console.WriteLine(nome2);
+    //Console.WriteLine(nome3);
+    //Console.WriteLine(nome4);
+
+   // Console.ReadKey();
